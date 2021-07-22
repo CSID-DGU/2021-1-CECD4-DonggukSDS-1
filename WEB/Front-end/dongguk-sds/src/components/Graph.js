c60742da6131da3790c9292248a9b200937b34e4
hyemdooly <hyemdooly@gmail.com>
2021-07-22 19:28:18 +0900

    nivo (graph) 적용

--- WEB/Front-end/dongguk-sds/src/components/Graph.js
+++ WEB/Front-end/dongguk-sds/src/components/Graph.js
@@ -1,15 +1,23 @@
 import { AiFillSetting } from 'react-icons/ai';
+import { ResponsiveBar } from "@nivo/bar";
 function Graph() {
+    const data = [
+        { quarter: 1, earnings: 13000 },
+        { quarter: 2, earnings: 16500 },
+        { quarter: 3, earnings: 14250 },
+        { quarter: 4, earnings: 19000 }
+      ];
     return (
-        <div className="w-max mb-6 rounded-lg border border-gray-300 p-6">
-            <div className="flex flex-row items-center justify-between">
+        <div className="w-max h-auto mb-6 rounded-lg border border-gray-300 p-6 resize overflow-auto">
+            <div className="flex flex-row items-center justify-between mb-3">
                 <p className="font-bold text-base">신공학관 전체 전기 사용량</p>
                 <AiFillSetting className="text-gray-400" size="20"/>
             </div>
-            
-            <div>
-                그래프 들어갈 자리 그래프 들어갈 자리 그래프 들어갈 자리 그래프 들어갈 자리 
+            <div className="flex h-4/6">
+                <ResponsiveBar data={data} keys={["earnings"]} indexBy="quarter" />
             </div>
+            
+
 
         </div>
     );
